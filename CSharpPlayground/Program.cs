19beea25546d5e1f5a74af97662eb1afbc22fc09
John Walshe <walshejohnnyw7@gmail.com>
2024-10-03 14:21:28 +0100

    Initial commit

---
+++ CSharpPlayground/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyFirstCSharpProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine(); // Wait for user input before closing
+        }
+    }
+}
